2909c05ebdc81ab7c6dd9484b5cae9852bc68c8c
1uvsic <1uvsic@naver.com>
2025-01-18 14:58:38 +0900

    backup

--- src/App.js
+++ src/App.js
@@ -1,59 +1,89 @@
-/* eslint-disable*/ 
+/* eslint-disable*/
 
-import { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import './App.css';
-import data from './data'
+import { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import "./App.css";
+import data from "./data.js";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import Project from "./Pages/Project.js";
 
 function App() {
+  let [otavi] = useState(data);
+  let navigate = useNavigate();
 
-  let [otavi] = useState(data)
-  
   return (
     <div className="App">
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home" className="head-logo">3hee ☘️</Navbar.Brand>
+          <Navbar.Brand href="#home" className="head-logo" onClick={()=>navigate('/')}>3hee ☘️</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#main" className="head-item">Main</Nav.Link>
-            <Nav.Link href="#Project" className="head-item">Project</Nav.Link>
-            <Nav.Link href="#Emoticon" className="head-item">Emoticon</Nav.Link>
+
+            <Nav.Link onClick={()=>navigate('/')}>Home</Nav.Link>
+            <Nav.Link onClick={()=>navigate('/project')}>Project</Nav.Link>
+            <Nav.Link onClick={()=>navigate('/emoticon')}>Emoticon</Nav.Link>
+            {/* <Link to="/" className="head-item">
+              Main
+            </Link>
+            <Link to="/project" className="head-item">
+              Project
+            </Link>
+            <Link to="/emoticon" className="head-item">
+              Emoticon
+            </Link> */}
+
+
           </Nav>
         </Container>
       </Navbar>
 
-      <div className="main-bg"></div>
 
-      <div className="container">
-        <div className="row">
-          {
-            otavi.map((a, i)=>{
-              return (
-                <MainItem otavi ={otavi[i]} i={i}></MainItem>
-              )
-            })
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <div className="container">
+                <div className="row">
+                  {otavi.map((a, i) => {
+                    return <MainItem otavi={otavi[i]} i={i}></MainItem>;
+                  })}
+                </div>
+              </div>
+            </>
           }
-        </div>
-      </div>
-      </div>
-
+        />
+        {/* <Route path="*" element={<div>404 Error! </div>} /> */}
+        <Route path="/project/:id" element={<Project otavi={otavi} />} />
+        <Route path="/emoticon" element={<Emoticon/>}   />
+      </Routes>
+    </div>
   );
+}
+
+function Emoticon() {
+  return (
+    <div>
+      <h4>이모티콘 소개페이지🫠</h4>
+    </div>
+  )
 }
 
 function MainItem(props) {
   return (
     <div className="col-md-4">
-    <img src={`${process.env.PUBLIC_URL}/0${props.i+1}.png`} 
-    alt={`Emoticon_${props.i}`} 
-    className="img-fluid" 
-    width="70%" 
-    />
-    <h4>{props.otavi.title}</h4>
-    <p>{props.otavi.content}</p>
-  </div>
+      <img
+        src={`${process.env.PUBLIC_URL}/0${props.i + 1}.png`}
+        alt={`Emoticon_${props.i}`}
+        className="img-fluid"
+        width="70%"
+      />
+      <h4>{props.otavi.title}</h4>
+      <p>{props.otavi.content}</p>
+    </div>
   );
 }
 
